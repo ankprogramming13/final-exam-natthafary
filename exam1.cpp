fb3c9d1cbfd2d3201547ae320ac5212e071bf438
Unknown <farynanmonot@gmail.com>
2018-02-22 12:29:20 +0700

    1-2

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
+        int cmd;
 
         return 0;
 }
--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <stack>
+suing namespace std;
 
 int main() {
+    int cmd;
+    string name;
+    string line;
+    ofstream writefile;
+    ifstream readfile;
 
+    do{
+        cout << "MENU" << endl;
+        cout << "1. Add student." << endl;
+        cout << "2. List student." << endl;
+        cout << "3. Search student." << endl;
+        cout << "4. Quit" << endl;
+        cout << "Please enter menu :";
+        cin >> cmd;
+
+        if (cmd == 1){
+            cout << "Enter Std name:";
+            cin >> name;
+
+        writefile.open("std.txt",fatream::app);
+            if(writefile.is_open()){
+                writefile << name << endl;
+            }else{
+            cout << "unable to open file "
+            }
+        }
+    }
         return 0;
 }
